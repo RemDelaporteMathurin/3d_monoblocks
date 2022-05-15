b78047db0423bfc7705060abb3d1b8367aea612b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-15 12:22:59 +0200

    better plotting

--- standard_case/plot_desorption_flux_over_time.py
+++ standard_case/plot_desorption_flux_over_time.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import matplotx
 
 
-data_recomb = np.genfromtxt("instant_recomb/derived_quantities.csv", delimiter=",", names=True)
+data_recomb = np.genfromtxt(
+    "instant_recomb/derived_quantities.csv", delimiter=",", names=True
+)
 
 id_poloidal_gap = 11
 id_top_pipe = 12
@@ -16,7 +18,7 @@ thickness = 4e-3  # m
 width = 23e-3  # m
 top_area = thickness * width  # m2
 retro_desorbed_flux = 1.61e22  # H/m2/s
-retro_desorbed_flux *= top_area/4  # H
+retro_desorbed_flux *= top_area / 4  # H
 
 t = data_recomb["ts"]  # time in s
 
@@ -26,7 +28,7 @@ flux_toroidal_gap = -data_recomb["Flux_surface_{}_solute".format(id_toroidal_gap
 flux_top_pipe = -data_recomb["Flux_surface_{}_solute".format(id_top_pipe)]
 flux_bottom = -data_recomb["Flux_surface_{}_solute".format(id_bottom)]
 flux_coolant = -data_recomb["Flux_surface_{}_solute".format(id_coolant)]
-flux_retro = np.ones(t.shape)*retro_desorbed_flux
+flux_retro = np.ones(t.shape) * retro_desorbed_flux
 
 # multiply the fluxes by 4 to account for the whole MB
 flux_poloidal_gap *= 4
@@ -47,6 +49,7 @@ with plt.style.context(matplotx.styles.dufte):
 
     matplotx.line_labels()
     matplotx.ylabel_top("Desorption \n flux (H/s)")
+    plt.ticklabel_format(axis="x", style="sci", scilimits=(0, 0))
     plt.xlabel("Time (s)")
     plt.ylim(top=1e19)
     plt.xlim(left=0)
