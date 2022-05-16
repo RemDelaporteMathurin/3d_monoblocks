8f1d9599422cef03d8369edb85b9826ec6a86467
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-16 16:15:50 +0200

    add scripts for case without CuCrZr extrusion

---
+++ results/plot_average_T_coolant.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotx
+
+thicknesses = [4, 5, 6, 7, 8, 9, 10, 14]
+avg_Ts_no_gap = []
+avg_Ts_gap = []
+time = 1e4
+id_coolant = 10
+for thickness in thicknesses:
+    folder = "{}mm_thickness/transient/instant_recomb/".format(thickness)
+
+    data_gap = np.genfromtxt(
+        "{}/derived_quantities.csv".format(folder), delimiter=",", names=True
+    )
+    data_no_gap = np.genfromtxt(
+        "{}/no_gap/derived_quantities.csv".format(folder), delimiter=",", names=True
+    )
+    index = np.where(data_gap["ts"] == time)[0][0]
+    if index != 0:
+        raise ValueError(
+            "More than one entry found in derived quantities, please adapt code."
+        )
+    avg_Ts_gap.append(data_gap["Average_T_surface_{}".format(id_coolant)])
+    avg_Ts_no_gap.append(data_no_gap["Average_T_surface_{}".format(id_coolant)])
+
+
+with plt.style.context(matplotx.styles.dufte):
+    plt.plot(thicknesses, avg_Ts_gap, label="Gap")
+    plt.plot(thicknesses, avg_Ts_no_gap, label="No gap")
+    plt.ylim(bottom=323)
+    yticks = plt.gca().get_yticks()[:]
+    yticks_labels = [y for y in yticks]
+    yticks_labels[0] = "Coolant temp"
+    plt.yticks(yticks, yticks_labels)
+    matplotx.ylabel_top("Average temperature \n cooling surface (K)")
+    matplotx.line_labels()
+    plt.xlabel("Thickness $e$ (mm)")
+    plt.tight_layout()
+    plt.show()
---
+++ results/plot_inventory_no_gap.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotx
+
+transient = True
+thicknesses = [4, 5, 6, 7, 8, 9, 10, 14]
+retention_w = []
+retention_wo = []
+time = 1e4
+for instant_recomb in [True, False]:
+    for thickness in thicknesses:
+        folder = "{}mm_thickness".format(thickness)
+        if transient:
+            folder += "/transient"
+        else:
+            folder += "/steady_state"
+        if instant_recomb:
+            folder += "/instant_recomb"
+        else:
+            folder += "/no_recomb"
+        data = np.genfromtxt(
+            "{}/no_gap/derived_quantities.csv".format(folder), delimiter=",", names=True
+        )
+        index = np.where(data["ts"] == time)[0][0]
+        if index != 0:
+            raise ValueError(
+                "More than one entry found in derived quantities, please adapt code."
+            )
+        retention = sum(
+            [data["Total_retention_volume_{}".format(vol_id)] for vol_id in [6, 7, 8]]
+        ) / (thickness * 1e-3)
+        if instant_recomb:
+            retention_w.append(retention)
+        else:
+            retention_wo.append(retention)
+
+ratio = [w / wo for w, wo in zip(retention_w, retention_wo)]
+difference = [100 * abs(w - wo) / w for w, wo in zip(retention_w, retention_wo)]
+
+with plt.style.context(matplotx.styles.dufte):
+    fig, axs = plt.subplots(2, 1, sharex=True, figsize=(6.4, 4.8 * 1.5))
+    plt.sca(axs[0])
+    plt.plot(
+        thicknesses,
+        retention_w,
+        color="tab:blue",
+        label="Instantaneous \n recombination",
+    )
+    plt.plot(thicknesses, retention_wo, color="tab:blue", label="No recombination")
+    plt.fill_between(thicknesses, retention_w, retention_wo, alpha=0.3)
+    matplotx.ylabel_top("Inventory per \n unit thickness \n (H/m) \n")
+    # plt.yscale("log")
+    plt.ylim(bottom=0)
+    matplotx.line_labels()
+    plt.tight_layout()
+
+    plt.sca(axs[1])
+
+    matplotx.ylabel_top("Relative \n difference (%)")
+    plt.xlabel("Thickness $e$ (mm)")
+    plt.plot(thicknesses, difference)
+    plt.ylim(bottom=0)
+    plt.tight_layout()
+    plt.show()
