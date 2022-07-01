5f4f5c3a671eab94a10a12a52b1a3a1bbc2786c0
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-07-01 09:04:31 +0000

    one plot instead of subplots

--- baking/plot_inventory.py
+++ baking/plot_inventory.py
@@ -1,56 +1,60 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import matplotx
+from labellines import labelLine, labelLines
 
 id_W = 6
 id_Cu = 7
 id_CuCrZr = 8
 
-baking_temperature = 600
-data = np.genfromtxt(
-    "baking_temperature={:.0f}K/derived_quantities.csv".format(baking_temperature),
-    delimiter=",",
-    names=True,
-)
 
-inventory = sum(
-    [
-        data["Total_retention_volume_{}".format(vol_id)]
-        for vol_id in [id_W, id_Cu, id_CuCrZr]
-    ]
-)
+def plot_inventory(baking_temperature, **kwargs):
 
-total_w = sum([data["Total_{}_volume_{}".format(trap_id, id_W)] for trap_id in [1, 2]])
-total_cu = data["Total_{}_volume_{}".format(2, id_Cu)]
-total_cucrzr = data["Total_{}_volume_{}".format(2, id_CuCrZr)]
-# total_mobile = sum(
-#     [
-#         data["Total_solute_volume_{}".format(mat_id)]
-#         for mat_id in [id_W, id_Cu, id_CuCrZr]
-#     ]
-# )
-
-relative_total_w = total_w / inventory[0]  # * 100
-relative_inventory = inventory / inventory[0]  # * 100
-# relative_mobile = total_mobile  # / retention[0]  # * 100
-
-with plt.style.context(matplotx.styles.dufte):
-    plt.plot(data["ts"], relative_inventory, marker="+")
-
-    plt.fill_between(
-        data["ts"],
-        np.zeros(relative_total_w.size),
-        relative_total_w,
-        alpha=0.3,
+    data = np.genfromtxt(
+        "baking_temperature={:.0f}K/derived_quantities.csv".format(baking_temperature),
+        delimiter=",",
+        names=True,
     )
-    # plt.fill_between(
-    #     data["ts"],
-    #     relative_total_w,
-    #     relative_total_w + relative_mobile,
-    #     alpha=0.3,
-    # )
-    plt.ylim(bottom=0)
-    plt.xlabel("Baking time (s)")
-    matplotx.ylabel_top("Relative \n inventory (%)")
-    plt.tight_layout()
-    plt.show()
+
+    inventory = sum(
+        [
+            data["Total_retention_volume_{}".format(vol_id)]
+            for vol_id in [id_W, id_Cu, id_CuCrZr]
+        ]
+    )
+
+    relative_inventory = inventory / inventory[0] * 100
+
+    plt.plot(
+        data["ts"] / 3600 / 24,
+        relative_inventory,
+        label="{} K".format(baking_temperature),
+        **kwargs
+    )
+
+
+if __name__ == "__main__":
+    with plt.style.context(matplotx.styles.dufte):
+        min_T_colour, max_T_colour = 500, 673
+        for T in [573, 600, 673]:
+            # plt.annotate(r"$T_\mathrm{baking}" + " = {} K$".format(T), (0.5e6, 60))
+            plot_inventory(
+                baking_temperature=T,
+                color=cm.Reds((T - min_T_colour) / (max_T_colour - min_T_colour)),
+            )
+
+        labelLines(
+            plt.gca().get_lines(),
+            yoffsets=0.01,
+            align=True,
+            backgroundcolor="none",
+            xvals=[7e5 / 3600 / 24, 3.4e5 / 3600 / 24, 3e5 / 3600 / 24],
+        )
+        # label axis
+        matplotx.ylabel_top("Relative \n inventory (%)")
+        plt.xlabel("Baking time (days)")
+        plt.ylim(bottom=-5)
+
+        plt.tight_layout()
+        plt.show()
