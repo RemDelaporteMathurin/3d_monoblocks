e1617e47fd9fd358362c87cac62e432339b81d24
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-07-01 15:11:52 +0000

    better plotting

--- baking/plot_inventory.py
+++ baking/plot_inventory.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 import matplotx
-from labellines import labelLine, labelLines
 
 id_W = 6
 id_Cu = 7
@@ -36,25 +35,19 @@ def plot_inventory(baking_temperature, **kwargs):
 
 if __name__ == "__main__":
     with plt.style.context(matplotx.styles.dufte):
-        min_T_colour, max_T_colour = 500, 673
-        for T in [573, 600, 673]:
-            # plt.annotate(r"$T_\mathrm{baking}" + " = {} K$".format(T), (0.5e6, 60))
+        plt.figure(figsize=(6.4, 6))
+        min_T_colour, max_T_colour = 400, 673
+        for T in [500, 673, 573, 550, 520, 600]:
             plot_inventory(
                 baking_temperature=T,
                 color=cm.Reds((T - min_T_colour) / (max_T_colour - min_T_colour)),
             )
 
-        labelLines(
-            plt.gca().get_lines(),
-            yoffsets=0.01,
-            align=True,
-            backgroundcolor="none",
-            xvals=[7e5 / 3600 / 24, 3.4e5 / 3600 / 24, 3e5 / 3600 / 24],
-        )
         # label axis
         matplotx.ylabel_top("Relative \n inventory (%)")
         plt.xlabel("Baking time (days)")
         plt.ylim(bottom=-5)
 
+        matplotx.line_labels()
         plt.tight_layout()
         plt.show()
